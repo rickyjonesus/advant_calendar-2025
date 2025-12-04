1eb99118e8ca002124a50d40f645f3e814028494
Ricky Jones <rickyjonesus@gmail.com>
2025-12-04 15:14:28 -0500

    chore: add additional real line code

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -1,5 +1,27 @@
+use std::fs::File;
+use std::io::{self, BufRead};
 
+
+
+struct Spot{
+    _has_roll: bool,
+    _adjunct_rolls: Vec<Vec<Spot>>
+}
+fn load_roles(_contents: Vec<String>) -> Vec<Vec<Spot>> {
+    return vec![];
+}
 
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./src/example-data.txt").expect("Unable to open file");
+    let reader = io::BufReader::new(file);
+    let mut contents = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+        contents.push(line);
+    }
+    
+
+    let _spots = load_roles(contents);
+    println!("done");
 }
