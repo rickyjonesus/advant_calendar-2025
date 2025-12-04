b93c56cfc6456c78d245fef42e058714b0030f1a
Ricky Jones <ricky.jones@transimpact.com>
2025-12-04 17:37:51 -0500

    feat: working ond debugging

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -4,11 +4,25 @@ use std::io::{self, BufRead};
 
 
 struct Spot{
-    _has_roll: bool,
-    _adjunct_rolls: Vec<Vec<Spot>>
+    has_roll: bool,
 }
+
 fn load_roles(_contents: Vec<String>) -> Vec<Vec<Spot>> {
-    return vec![];
+    let mut ret: Vec<Vec<Spot>> = vec![vec![]];
+    for line in _contents { 
+        let mut row = Vec::new();
+        for c in line.chars() {
+            print!("{} ", c);
+            let spot = Spot{
+                has_roll: if c == '@' { true } else { false },
+            };
+            row.push(spot);
+        }
+        println!("");
+        ret.push(row);
+    }
+
+    return ret;
 }
 
 fn main() {
@@ -17,11 +31,78 @@ fn main() {
     let mut contents = Vec::new();
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
         contents.push(line);
     }
     
+    let spots = load_roles(contents);
+    let mut rowIndex = 0;
+    let mut availableRolls = 0;
+    for row in spots.iter() {
 
-    let _spots = load_roles(contents);
-    println!("done");
+        let mut colIndex = 0;
+
+        for col in row.iter() {
+            let mut totalTouchingRolls = 0;
+            if(col.has_roll){
+            
+                //Directly before
+                if colIndex > 1 && spots[rowIndex][colIndex - 1].has_roll {
+                    totalTouchingRolls += 1;
+                 
+                }
+
+
+                //Directly after
+                if colIndex < row.len() - 1 && spots[rowIndex][colIndex + 1].has_roll {
+                 
+                    totalTouchingRolls += 1;
+                }
+                let beginRange = if colIndex > 1 { colIndex - 1 } else { 1 };
+                let endRange = if colIndex < row.len() - 1 { colIndex + 1 } else { row.len() - 1 };
+                //Directly Above
+                if rowIndex > 1
+                {                    
+                    for rel in beginRange..endRange {
+                        if spots[rowIndex - 1][rel].has_roll {
+                            totalTouchingRolls += 1;
+                        }
+                    }
+                }
+
+                //Directly Below
+                if rowIndex < spots.len() - 1 {
+                    print!("b{}-{}", beginRange, endRange);
+                    for rel in beginRange..endRange {
+
+                        if spots[rowIndex + 1][rel].has_roll {
+                            totalTouchingRolls += 1;
+                        }
+                    }
+                }
+
+                if totalTouchingRolls < 4{
+                    availableRolls += 1;
+                }
+                if totalTouchingRolls < 4
+                {
+                    print!("@ ");
+                }
+                else
+                {
+                    print!("x ");
+                }
+                print!("{}", totalTouchingRolls);
+
+            }
+            else {
+                print!(". ");
+            }   
+            colIndex += 1;
+        }
+        println!("");
+        rowIndex += 1;
+
+    }
+
+    println!("done {}", availableRolls);
 }
