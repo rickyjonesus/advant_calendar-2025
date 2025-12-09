62d151b3437803aa8a340d8f34ec0368f3fe6967
Ricky Jones <rickyjonesus@gmail.com>
2025-12-09 14:36:32 -0500

    add day 7 in

---
+++ Day7/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string filePath = "./example-data.txt";
+
+var lines = File.ReadAllLines(filePath);
+var linesList = lines.ToList();
+for (int i = 0; i < linesList.Count - 1; i++)
+{
+    var line = linesList[i].ToArray();
+    var nextLine = linesList[i + 1].ToArray();
+
+    for (int ci = 0; ci < line.Length; ci++)
+    {
+        var c = line[ci];
+        if(line[ci] == 'S' || line[ci] == '|')
+        {
+            nextLine[ci] = '|';
+        }
+        else if (line[ci] == '^' )
+        {
+            nextLine[ci-1] = '|';
+            nextLine[ci+1] = '|';
+            nextLine[ci] = '.';
+        }
+    }
+
+    Console.WriteLine(new string(line));
+
+}
