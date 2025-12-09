91c343f900def24271d4d55079fde1a212209a4a
Ricky Jones <ricky.jones@transimpact.com>
2025-12-09 15:55:36 -0500

    Day 1 working

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,30 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string filePath = "./example-data.txt";
+string filePath = "./data.txt";
 
 var lines = File.ReadAllLines(filePath);
-var linesList = lines.ToList();
-for (int i = 0; i < linesList.Count - 1; i++)
+var linesList = lines.Select(l => l.ToArray()).ToArray();
+int totalSplits = 0;
+
+int lastRowDecisinoPoint = linesList.Length - 1;
+for (int i = 0; i < linesList.Count() - 1; i++)
 {
-    var line = linesList[i].ToArray();
-    var nextLine = linesList[i + 1].ToArray();
+    var line = linesList[i];
+    var nextLine = linesList[i + 1];
 
     for (int ci = 0; ci < line.Length; ci++)
     {
         var c = line[ci];
-        if(line[ci] == 'S' || line[ci] == '|')
+        if (line[ci] == 'S' || line[ci] == '|')
         {
-            nextLine[ci] = '|';
-        }
-        else if (line[ci] == '^' )
-        {
-            nextLine[ci-1] = '|';
-            nextLine[ci+1] = '|';
-            nextLine[ci] = '.';
+     
+            if (nextLine[ci] == '^')
+            {
+                totalSplits++;
+                nextLine[ci - 1] = '|';
+                nextLine[ci + 1] = '|';
+            }
+            else  nextLine[ci] = '|';
+
         }
     }
+}
+foreach (var l in linesList)
+{
+    Console.WriteLine(new string(l));
+}
 
-    Console.WriteLine(new string(line));
+Console.WriteLine($"Total splits: {totalSplits}");
 
+class particleSplitter
+{
+    static void SplitParticles(char[][] linesList)
+    {
+        
+    }
 }
